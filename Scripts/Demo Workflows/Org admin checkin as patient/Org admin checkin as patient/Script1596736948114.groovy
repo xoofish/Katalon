10047c8b32e634f2d5ef13e56fa3edbb61c94a5c
Alex Cullor <Alex Cullor@DESKTOP-8E17IQ5>
2020-09-15 11:02:40 -0400

    Updated Org Admin Check-In as Patient with Password change

--- Scripts/Demo Workflows/Org admin checkin as patient/Org admin checkin as patient/Script1596736948114.groovy
+++ Scripts/Demo Workflows/Org admin checkin as patient/Org admin checkin as patient/Script1596736948114.groovy
@@ -25,7 +25,7 @@ WebUI.navigateToUrl('https://test-pp.wecounsel.com/users/sign_in')
 WebUI.setText(findTestObject('Page_User Login  WeCounsel/input_Please Log In_useremail'), 'nickbiz060+AutoOrgAdmin@gmail.com')
 
 'Password123'
-WebUI.setEncryptedText(findTestObject('Page_User Login  WeCounsel/input_Please Log In_userpassword'), 'p4y+y39Ir5PJb2ispxT0Ew==')
+WebUI.setEncryptedText(findTestObject('Page_User Login  WeCounsel/input_Please Log In_userpassword'), 'LCsxB9VPamtRlQlpiLe+cw==')
 
 WebUI.click(findTestObject('Page_User Login  WeCounsel/button_Log in'))
 
