516e25c3ae1a39b21bcb3e30df999a7be2400522
cdesoto <carl.desoto@gmail.com>
2021-08-02 14:21:40 -0500

    Remote Automation update for Katalon DevOps

---
+++ Test Suites/Active Suites/Prod Testing.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Prod Testing</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>5</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>bf9e5082-ca61-4ac0-91a9-a961fb446cc9</testSuiteGuid>
+   <testCaseLink>
+      <guid>6010c3d8-1902-485b-a8e4-d0cd7a55faa5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Active Tests/Live Pexip Check</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
